326fecb22855225cd3cd7302607556e64ce31ebe
powerstar7696-afk <powerstar7696@gmail.com>
2026-06-16 15:59:30 +0530

    fix: unique name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-secure-tf-state-235423434"
+    bucket         = "my-secure-tf-state-16062026"
     key            = "terraform/state"
     region         = "eu-west-1"
     encrypt        = true
